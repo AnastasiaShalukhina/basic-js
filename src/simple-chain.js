fa4abac62eca6ca06897272e4db9eb8154ad43bc
AnastasiaShalukhina <exachka4@gmail.com>
2022-10-11 10:00:02 +0300

    add solution for simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,52 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
 const chainMaker = {
+  arr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (typeof value === "undefined") {
+      this.arr.push(`( )~~`);
+    } else {
+      this.arr.push(`( ${value} )~~`);
+    }
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (
+      isNaN(position) ||
+      !Number.isInteger(position) ||
+      position > this.arr.length - 1 ||
+      position <= 0
+    ) {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    } else {
+      this.arr.splice(position - 1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let elemToModif = this.arr[this.arr.length - 1]; // string
+    let corrElem = elemToModif.substring(0, elemToModif.length - 2);
+    this.arr.pop();
+    this.arr.push(corrElem);
+    let str = this.arr.join("");
+    this.arr = [];
+    return str;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
