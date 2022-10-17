abad92c3bb61a1d2faefa92673ecb78982d3475e
AnastasiaShalukhina <exachka4@gmail.com>
2022-10-17 10:07:30 +0300

    feat: add solution for delete-digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,7 +11,19 @@ const { NotImplementedError } = require("../extensions/index.js");
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(n) {}
+function deleteDigit(n) {
+  let arrWithDigits = n.toString().split("");
+  let arrWithCuttedNum = [];
+  for (let i = 0; i < arrWithDigits.length; i++) {
+    let arrWithDigitsCopy = arrWithDigits.slice(0);
+    arrWithDigitsCopy.splice(i, 1);
+    let strCuttedNumber = arrWithDigitsCopy.join("");
+    arrWithCuttedNum.push(strCuttedNumber);
+  }
+  arrWithCuttedNum.sort((a, b) => b - a);
+  let biggerNum = +arrWithCuttedNum[0];
+  return biggerNum;
+}
 
 module.exports = {
   deleteDigit,
